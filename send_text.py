7b15d110876a6fdad19ebd56f2bc60bf3752519a
Mrinalini Iyer <mrinalini17788@gmail.com>
2018-04-02 09:05:46 -0700

    Update send_text.py

--- send_text.py
+++ send_text.py
@@ -1,15 +1,15 @@
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = "AC58930cf3320c925198af67c3fec54f2a"
+account_sid = "******"
 # Your Auth Token from twilio.com/console
-auth_token  = "6a2911dcbb24b3cc58eda54735dec6df"
+auth_token  = "***"
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+19496103089", 
-    from_="+15626081073",
-    body="I love you !")
+    to="***, 
+    from_="+15626****",
+    body="Text message from Twilio : python!")
 
 print(message.sid)
